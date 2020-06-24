3827ac51ecefd192f5167ed7c18e5ce868445eeb
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-24 00:30:21 +0000

    [add]board model asociation

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,11 +1,14 @@
-# frozen_string_literal: true
-
 class Board < ApplicationRecord
   belongs_to :user
+  belongs_to :board_tag
 
   attachment :image
 
   validates :title, presence: true, length: { minimum: 2, maximum: 200 }
   validates :subject, presence: true, length: { minimum: 2 }
   validates :body, presence: true, length: { maximum: 2000 }
+  validates :board, inclusion: {in: [true, false]}
+
+
 end
+
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,9 @@
 class Tag < ApplicationRecord
+	has_many :board_tag
+
+	validates :name, presence: true
+	validates :is_void, inclusion: {in: [true, false]}
+
 end
+
+
