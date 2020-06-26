a6a951bc5b9b4acfe4836dd616991bae000d299f
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-26 22:25:42 +0000

    [add]Tag/fuction

--- app/controllers/manage/tags_controller.rb
+++ app/controllers/manage/tags_controller.rb
@@ -5,8 +5,19 @@ class Manage::TagsController < ApplicationController
   	@tag = Tag.new
   end
 
+  def create
+  	@tag = Tag.new(tag_params)
+  	if @tag.save
+  		redirect_to manage_tags_path
+  		flash[:notice] = "タグを追加しました"
+  	else
+  		render :new
+  	end
+  end
+
   def index
-  	@tags = Tag.all.page(params[:page]).per(20)
+  	@tag = Tag.new
+  	@tags = Tag.all.order(created_at: :desc).page(params[:page]).per(20)
   end
 
   def edit; end
@@ -14,4 +25,10 @@ class Manage::TagsController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def tag_params
+  	params.require(:tag).permit(:name, :is_void)
+  end
 end
