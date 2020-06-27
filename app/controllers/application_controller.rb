8098bbe9c29e43a00426ef046487437801beb3de
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-27 01:32:15 +0000

    [add]flash-message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session
+  add_flash_types :success, :info, :warning, :danger
 
   protected
 
