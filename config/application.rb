e1462ee3e1e349160ac585d3e37e5a2f5e3f07ab
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-29 03:02:56 +0000

    [add]lang=ja/setting

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,11 @@ module StudyRecord
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja # タイムゾーンの設定
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+  end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
 end
