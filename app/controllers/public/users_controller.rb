8ca138a71866d0e7e1bd5fcbb34d7ed8dca41432
makiharu <kitakazetoshirokuma@gmail.com>
2020-06-30 03:10:12 +0000

    [add]follows/followers.count/restriction

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,14 +6,7 @@ class Public::UsersController < ApplicationController
     @users = User.all
   end
 
-  def show
-    # @todolist = Todolist.new
-    # @todolists = Todolist.all
-    # if params[:todolist_id]
-    #   @finish_list = Todolist.find(params[:todolist_id])
-    #   @todolists = Todolist.all
-    # end
-  end
+  def show; end
 
   def edit; end
 
@@ -31,7 +24,7 @@ class Public::UsersController < ApplicationController
   def follows
     user = User.find(params[:id])
     @users = user.followings
-  end
+ end
 
   def followers
     user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,33 +15,38 @@ class User < ApplicationRecord
   has_many :todolists, dependent: :destroy
   has_many :board_likes, dependent: :destroy
 
-  #follow-follower
+  # follow-follower
   has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id
-  has_many :follows, through: :active_relationships, source: :follower
+  has_many :followings, through: :active_relationships, source: :follower
 
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
-
 
   attachment :profile_image
 
   validates :name, presence: true
 
   def followed_by?(user)
-# フォローされているpassiveユーザーの中から、引数に渡されたuserがいるかどうかjudge
+    # フォローされているpassiveユーザーの中から、引数に渡されたuserがいるかどうかjudge
     passive_relationships.find_by(following_id: user.id).present?
   end
 
-
   def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
+    #user = User.where(uid: auth.uid, provider: auth.provider).first
 
-    user ||= User.create(
+    # user ||= User.create(
+    #   uid: auth.uid,
+    #   provider: auth.provider,
+    #   email: auth.info.email,
+    #   password: Devise.friendly_token[0, 20]
+    # )
+    User.where(uid: auth.uid, provider: auth.provider).first
+
+    User.create(
       uid: auth.uid,
       provider: auth.provider,
       email: auth.info.email,
       password: Devise.friendly_token[0, 20]
     )
   end
-
 end
