2bbe22c8321f076a44fc1909ac0b99577ad31820
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-11 13:17:37 +0000

    [add]user-model/introduction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_031426) do
+ActiveRecord::Schema.define(version: 2020_07_11_131621) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 2020_07_09_031426) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
