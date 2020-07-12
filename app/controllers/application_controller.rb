6eb44727bf520c6258ccd75a950f46b9036cca0c
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-12 04:37:56 +0000

    [fix]routing-error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Admin
-      manage_home_top_path
+      home_top_path
     when User
       user_path(current_user)
     end
