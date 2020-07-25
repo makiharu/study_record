fdb7d0e8dfe0d545ddab2c7fa4a2231589015bba
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-25 14:50:27 +0000

    [add]board-comment-test

--- spec/models/board_comment_spec.rb
+++ spec/models/board_comment_spec.rb
@@ -11,19 +11,5 @@ RSpec.descirbe 'BoardCommentモデルのテスト', type: :model do
 				expect(board_comment.valid?).to eq false;
 			end
 		end
+	end
 end
-
-
-
-
-
-  # belongs_to :user
-  # belongs_to :board
-  # has_many :comment_likes, dependent: :delete_all
-
-  # validates :comment, presence: true, length: { maximum: 1500 }
-
-  # # 引数で渡されたユーザidがテーブル内に存在するかどうか判定
-  # def comment_liked_by?(user)
-  #   comment_likes.where(user_id: user.id).exists?
-  # end
