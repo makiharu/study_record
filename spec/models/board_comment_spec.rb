22b6f13527598e079c9d6cbbe97080de8a325620
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-25 16:00:56 +0000

    [add]test

--- spec/models/board_comment_spec.rb
+++ spec/models/board_comment_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
-RSpec.descirbe 'BoardCommentモデルのテスト', type: :model do
+RSpec.describe 'BoardCommentモデルのテスト', type: :model do
 	describe 'バリデーションのテスト' do
 		let(:user) { create(:user) }
-		let(:board) { build(:board, user_id: user.id) }
+		let(:board) { create(:board, user_id: user.id) }
+		let(:board_comment) { build(:board_comment, user_id: user.id, board_id: board.id) }
 
 		context 'commentカラム' do
 			it '空欄でないこと' do
-				comment.content = ''
+				board_comment.content = ''
+				expect(board_comment.valid?).to eq false;
+			end
+
+			it '1500文字以下であること' do
+				board_comment.content = Faker::Lorem.characters(number:1501)
 				expect(board_comment.valid?).to eq false;
 			end
 		end
--- test/controllers/public/board_comments_controller_test.rb
+++ test/controllers/public/board_comments_controller_test.rb
@@ -3,13 +3,13 @@
 require 'test_helper'
 
 class Public::BoardCommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get public_board_comments_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get public_board_comments_index_url
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get public_board_comments_edit_url
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get public_board_comments_edit_url
+  #   assert_response :success
+  # end
 end
